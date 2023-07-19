8a5619e556ab13d0a4c93b87faad2bf118999096
gunhee98 <127311862+gunhee98@users.noreply.github.com>
2023-07-19 19:46:42 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,4 +1,4 @@
-function solution(arr) {
+/*function solution(arr) {
     var answer = [];
     let min=arr[0];
     arr.forEach((i)=>{
@@ -7,4 +7,16 @@ function solution(arr) {
     answer=arr.filter(i=>i!==min);
     
     return answer.length?answer:[-1];
+}
+*/
+
+2
+3
+4
+5
+6
+function solution(arr) {
+    arr.splice(arr.indexOf(Math.min(...arr)),1);
+    if(arr.length<1)return[-1];
+    return arr;
 }
