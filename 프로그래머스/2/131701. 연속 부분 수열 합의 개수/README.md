01d23570f9133bb9d14122b8f4f9e2cc8f7e9270
gunhee98 <127311862+gunhee98@users.noreply.github.com>
2023-12-28 19:34:28 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 120.47 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=javascript) 
 
 ### 성능 요약
 
-메모리: 52 MB, 시간: 791.62 ms
+메모리: 56.7 MB, 시간: 120.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 4일 19:21:34
+2023년 12월 4일 19:34:29
 
 ### 문제 설명
 
--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -2,10 +2,10 @@ function solution(elements) {
     var answer = 0;
     const set=new Set();
     const len=elements.length;
-    const arr=[...elements,...elements.slice(0,len-1)];
-    for(let i=1;i<=len;i++){
-        for(let j=0;j<len;j++){
-            const sum=arr.slice(j,j+i).reduce((a,b)=>a+b,0);
+    for(let i=0;i<len;i++){
+        let sum=0;
+        for(let j=i;j<i+len;j++){
+            sum+=elements[j%len];
             set.add(sum);
         }
     }
